39b0495a08413ae4e9ea42ee85ec68d361c41f43
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-06-05 11:24:37 +0300

    init comment

---
+++ src/main/java/com/javaded/DataStoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javaded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStoreServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaded/DataStoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
